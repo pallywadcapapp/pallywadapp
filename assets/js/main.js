fd611b7173ae4f0ba0c3f891d8a904010c308404
Oduwole Oluwasegun <segxy2708@yahoo.com>
2024-01-28 06:57:18 +0100

    Update main.js
    upload profile doc

--- assets/js/main.js
+++ assets/js/main.js
@@ -371,6 +371,9 @@ function addLoan(email){
             }*/
             if(d.message == "upload all required documents profile"){
                 //redirect to document upload page
+                let message = `You need to update your profile in order to add a loan. 
+                <a href="upload-profile-documents">Click here</a> to update now.`;
+                $('#stepsNotification').html(message).show();
             }else if(d.message != true){
                 let message = `You need to update your profile in order to add a loan. 
                 <a href="update-profile">Click here</a> to update now.`;
